37a29569c2306c4396ec0840f4a2daa7b0e445cb
s4m77 <s.goldie@student.maastrichtuniversity.nl>
2022-03-14 10:19:21 +0100

    Update GolfBall.java

--- Golf-Phase1/src/GolfBall.java
+++ Golf-Phase1/src/GolfBall.java
@@ -1,8 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.Buffer;
+
 public class GolfBall {
     
-    double x, y, radius, speed;
+    double x, y, radius, speed, momentum;
 
-    
+
+    GolfBall(int x, int y){
+        this. x = x; this.y = y;
+        //radius = 2.13;
+        speed = 0;
+        input = new double[12];
+        readFile();
+    }
+    //set speed aof ball
+    public void setSpeed(double speed){
+        this.speed = speed;
+    }
+    //move a ball to a given position
+    public void moveBall(double x, double y){
+        this.x = x; this.y = y;
+    }
+
+
+
+
 
     
 }
