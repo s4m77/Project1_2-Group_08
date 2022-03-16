c8449c1739c635a01f18debf778d1f0e8b7ead67
s4m77 <s.goldie@student.maastrichtuniversity.nl>
2022-03-16 16:35:41 +0100

    Update FileInputManager.java

--- Phase_1/core/src/com/mygdx/golf/FileInputManager.java
+++ Phase_1/core/src/com/mygdx/golf/FileInputManager.java
@@ -2,7 +2,6 @@ package com.mygdx.golf;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
@@ -12,28 +11,34 @@ import java.net.URL;
 
 public class FileInputManager {
 
-    static int shots;
-    String fileName;
-    BufferedReader read;
-    URL pathInput;
-    URL velocityInput;
+    public static int shots;
+    
+    private BufferedReader read;
+    private URL pathInput;
+    private URL pathVelocity;
+
     /**
-     * array storing the input.txt file values
+     * array storing the input.txt file values and velocity.txt values
      * stores, in order: [0]:x0, [1]:y0, [2]:xt, [3]:yt, [4]:r, [5]:muk, [6]:mus
      * [7]:heightProfile, [8]:sandPitX, [9]:sandPitY, [10]:muks, [11]:muss
+     * [12]:v0x, [13]:v0y
      */
-    String[] input;
+    protected String[] input;
+    /**
+     * Constructor for a FileInputManager obj
+     */
     public FileInputManager(){
         shots = 0;
         this.pathInput = FileInputManager.class.getResource("input.txt");
-        this.velocityInput = FileInputManager.class.getResource("velocity.txt");
+        this.pathVelocity = FileInputManager.class.getResource("velocity.txt");
         
         try {readFile();} 
         catch (IOException e) {e.printStackTrace();}
     }
-
-    public String[] getInputArray() {return this.input;}
-
+    /**
+     * method responsible for reading and storing the input values
+     * @throws IOException if file not found 
+     */
     public void readFile() throws IOException {
         input = new String[14]; 
         read = new BufferedReader(new FileReader(new File(pathInput.getFile())));
@@ -52,7 +57,7 @@ public class FileInputManager {
                 }
             }
         }
-        read = new BufferedReader(new FileReader(new File(velocityInput.getFile())));
+        read = new BufferedReader(new FileReader(new File(pathVelocity.getFile())));
         for (int i = 12; i < input.length; i++) {
             String s = read.readLine();
             String s1;
@@ -71,6 +76,7 @@ public class FileInputManager {
         read.close();
     }
     //ACCESSORS
+    public String[] getInputArray() {return this.input;}
     public double getInitialX()      {return Double.parseDouble(input[0]);}
     public double getInitialY()      {return Double.parseDouble(input[1]);}
     public double getTargetX()       {return Double.parseDouble(input[2]);}
@@ -81,7 +87,12 @@ public class FileInputManager {
     public String getHeightProfile() {return input[7];}
     public double sandKinetic()      {return Double.parseDouble(input[10]);}
     public double sandStatic()       {return Double.parseDouble(input[11]);}
-    public double[] sandPitX(){
+    
+    /**
+     * this method translates an input of, e.g., -1<x<1
+     * @return arr[0]: beginning; arr[1] end
+     */
+    public double[] sandPitX(){            
         double[] result = new double[2];
         char[] s = input[8].toCharArray();
         for (int i = 0; i < s.length; i++) {
@@ -97,6 +108,10 @@ public class FileInputManager {
         }
         return result;
     }
+    /**
+     * this method translates an input of, e.g., -1<y<1
+     * @return arr[0]: beginning; arr[1] end
+     */
     public double[] sandPitY(){
         double[] result = new double[2];
         char[] s = input[9].toCharArray();
@@ -131,7 +146,7 @@ public class FileInputManager {
         //update current x and y in input array
         this.input[1] = initialX.toString(); this.input[2] = initialY.toString();
         //update the new velocity in input array
-        read = new BufferedReader(new FileReader(new File(velocityInput.getFile())));
+        read = new BufferedReader(new FileReader(new File(pathVelocity.getFile())));
         for (int i = 12; i < input.length; i++) {
             String s = read.readLine();
             String s1;
