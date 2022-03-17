22cac9ede3221d80bd784dca56f76ff06aaa7fac
s4m77 <s.goldie@student.maastrichtuniversity.nl>
2022-03-17 23:16:56 +0100

    Update DesktopLauncher.java

--- Phase_1/desktop/src/com/mygdx/golf/DesktopLauncher.java
+++ Phase_1/desktop/src/com/mygdx/golf/DesktopLauncher.java
@@ -3,7 +3,6 @@ package com.mygdx.golf;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
